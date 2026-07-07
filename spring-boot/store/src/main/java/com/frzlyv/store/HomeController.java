a88c842141cd33d3217d8116c756e9e3cf9e8889
NureddinFarzaliyev <nureddin.fa@gmail.com>
2026-07-07 09:03:12 +0400

    Return text instead of html

--- spring-boot/store/src/main/java/com/frzlyv/store/HomeController.java
+++ spring-boot/store/src/main/java/com/frzlyv/store/HomeController.java
@@ -1,21 +1,16 @@
 package com.frzlyv.store;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * HomeController
  */
-@Controller
+@RestController
 public class HomeController {
 
-  @Value("${spring.application.name}")
-  private String appName;
-
-  @RequestMapping("/")
-  public String index() {
-    System.out.println("App Name: " + appName);
-    return "index.html";
+  @GetMapping(path = "/hello")
+  public String helloWorld() {
+    return "Hello, World!";
   }
 }
